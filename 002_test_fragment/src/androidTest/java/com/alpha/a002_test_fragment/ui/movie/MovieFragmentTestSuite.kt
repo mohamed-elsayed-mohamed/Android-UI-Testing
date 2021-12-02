6386701bc090303b251df2a9998059402110b6c9
Mohamed.Hasanen <m.elsayed.m.1995@gmail.com>
2021-12-02 10:48:41 +0200

    Fragment Navigation Test

--- 002_test_fragment/src/androidTest/java/com/alpha/a002_test_fragment/ui/movie/MovieFragmentTestSuite.kt
+++ 002_test_fragment/src/androidTest/java/com/alpha/a002_test_fragment/ui/movie/MovieFragmentTestSuite.kt
@@ -4,5 +4,10 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
-@Suite.SuiteClasses(MovieDetailFragmentTest::class, DirectorsFragmentTest::class, StarActorsFragmentTest::class)
+@Suite.SuiteClasses(
+    MovieDetailFragmentTest::class,
+    DirectorsFragmentTest::class,
+    StarActorsFragmentTest::class,
+    MovieNavigationTest::class
+)
 class MovieFragmentTestSuite
---
+++ 002_test_fragment/src/androidTest/java/com/alpha/a002_test_fragment/ui/movie/MovieNavigationTest.kt
@@ -0,0 +1,35 @@
+package com.alpha.a002_test_fragment.ui.movie
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.alpha.a002_test_fragment.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MovieNavigationTest {
+    @Test
+    fun text_movieFragmentNavigation() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        // Nav DirectorsFragment
+        onView(withId(R.id.movie_directiors)).perform(click())
+
+        // Verify
+        onView(withId(R.id.fragment_movie_directors_parent)).check(matches(isDisplayed()))
+
+        pressBack()
+
+        onView(withId(R.id.fragment_movie_detail_parent)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.movie_star_actors)).perform(click())
+
+        onView(withId(R.id.fragment_movie_star_actors_parent)).check(matches(isDisplayed()))
+    }
+}
